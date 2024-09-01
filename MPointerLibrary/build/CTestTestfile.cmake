7fa5561ded0771c90bcc680f476aa5fb4b902a27
a-alfaro19 <134029114+a-alfaro19@users.noreply.github.com>
2024-09-01 14:34:02 -0600

    Delete MPointerLibrary/build directory

--- MPointerLibrary/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/andres/Documents/GitHub/MPointers/MPointerLibrary
-# Build directory: /home/andres/Documents/GitHub/MPointers/MPointerLibrary/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[Google_Tests_run]=] "/home/andres/Documents/GitHub/MPointers/MPointerLibrary/build/Google_Tests_run")
-set_tests_properties([=[Google_Tests_run]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/andres/Documents/GitHub/MPointers/MPointerLibrary/CMakeLists.txt;34;add_test;/home/andres/Documents/GitHub/MPointers/MPointerLibrary/CMakeLists.txt;0;")
-subdirs("/home/andres/Documents/GitHub/MPointers/MainApp/cmake-build-debug/_deps/googletest-build")
